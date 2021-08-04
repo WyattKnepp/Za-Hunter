b9f97211ba016f8a0cad93a1470276cf4a0b2c97
Wyatt Knepp <wyattjoeknepp@gmail.com>
2021-08-04 18:02:02 -0500

    Initial Commit

---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by Wyatt Knepp on 8/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
